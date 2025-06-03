3cb4d2b2bd48cc7e8c63d0b0c5531c4a4d7341a4
abhilash <abhimadaparambath@gmail.com>
2025-06-04 03:27:21 +0530

    notificaion add 11

--- server/server.js
+++ server/server.js
@@ -23,6 +23,9 @@ app.use(bodyParser.json());
 // Parse Firebase Admin SDK JSON from environment variable
 const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
+// Replace escaped newlines in private_key
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
