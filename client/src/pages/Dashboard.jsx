aab2ae12bbd6173d2257a993ab9c082f9398d1e2
abhilash <abhimadaparambath@gmail.com>
2025-06-04 17:19:18 +0530

    on off status updated 3

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -7,12 +7,12 @@ const socket = io("https://moto-app-test.onrender.com", {
 
 const Dashboard = () => {
   const [sensor, setSensor] = useState({});
-  const [motorStatus, setMotorStatus] = useState("STOP_PLANT");
+  const [motorStatus, setMotorStatus] = useState("OFF");
   const [motorNumber, setMotorNumber] = useState(1);
 
   const togglePump = () => {
-    const newStatus = motorStatus === "START_PLANT" ? "STOP_PLANT" : "START_PLANT";
-    socket.emit("motor_control", { command: newStatus });
+    const newCommand = displayMotorStatus === "ON" ? "STOP_PLANT" : "START_PLANT";
+    socket.emit("motor_control", JSON.stringify({ command: newCommand }));
   };
 
   useEffect(() => {
@@ -197,16 +197,16 @@ const Dashboard = () => {
             <button
               onClick={togglePump}
               className={`mt-2 sm:mt-0 cursor-pointer px-2 sm:px-4 py-1 sm:py-2 rounded text-xs sm:text-base font-medium text-white hover:opacity-90 transition-opacity ${
-                motorStatus === "START_PLANT" ? "bg-green-600" : "bg-red-500"
+                displayMotorStatus === "ON" ? "bg-green-600" : "bg-red-500"
               }`}
             >
-              {motorStatus === "START_PLANT" ? "Turn OFF" : "Turn ON"}
+              {displayMotorStatus === "ON" ? "Turn OFF" : "Turn ON"}
             </button>
           </div>
 
           <div className="flex flex-col p-2 sm:p-4 bg-gray-50 rounded shadow-sm">
             <span className="font-semibold mb-2">Water Levels</span>
-            <div>GLR (%): {sensor.water_level != null ? sensor.water_level : "N/A"}</div>
+            <div>GLR (%): {sensor.water_level_glr != null ? sensor.water_level_glr : "N/A"}</div>
             <div>OHT (%): {sensor.water_level_oht != null ? sensor.water_level_oht : "N/A"}</div>
             <div>
               Vacuum Switch: {sensor.vacuum_switch_ok === 1 ? "OK" : "NOT OK"}
