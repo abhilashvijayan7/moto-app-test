6aaa9248970a71dba591811d60aa031cfeb2ac17
abhilash <abhimadaparambath@gmail.com>
2025-08-01 15:17:21 +0530

    .env updated 2

--- server/server.js
+++ server/server.js
@@ -377,3 +377,6 @@ const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+// jhgfhjsgdfhjgsdfhjgsdfhjgsdhjfgsfhjgsfjhgsfhjgfhjsgfhjsfgsjhdfgdsjfgsdjhfgsjhdfgsdf
